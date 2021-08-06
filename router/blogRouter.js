e8a70ad4ae2c5c75423f44bb7d211919213db774
AkhilMengi <56011418+AkhilMengi@users.noreply.github.com>
2021-08-06 13:21:01 +0530

    Add files via upload

--- router/blogRouter.js
+++ router/blogRouter.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const blogController = require('./../controllers/blogController')
+const router = express.Router();
 
+router.route('/').get(blogController.getAllBlog).post(blogController.createNewBlog);
+router.route('/new').get(blogController.createBlog)
+router.route('/:id').delete(blogController.deleteBlog).put(blogController.editBlogById)
+router.route('/edit/:id').get(blogController.editBlog)
+
+
+
+ module.exports = router;
