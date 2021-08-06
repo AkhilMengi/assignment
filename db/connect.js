bba419d71a4d67b949d069b48abb6bef77553d60
AkhilMengi <56011418+AkhilMengi@users.noreply.github.com>
2021-08-06 13:18:18 +0530

    Add files via upload

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/blog",{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
 
+}).then(()=>{
+    console.log('connected successfully');
+}).catch(()=>{
+    console.log(`OOPS! ${err}`)
+})
